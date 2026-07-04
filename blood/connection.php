2e4c5f03547c250eb844d41555cc66608044bf5b
Faisal Khan <faisalkhan7863030@gmail.com>
2026-07-04 10:00:53 +0530

    Remove success message on database connection
    Removed success message after database connection.

--- blood/connection.php
+++ blood/connection.php
@@ -9,6 +9,4 @@ $conn = mysqli_connect($host, $user, $password, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-echo "Database Connected Successfully!";
 ?>
